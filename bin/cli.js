87f452da64316a6f9477205f807718f29d1f19da
hugohua <baofen14787@gmail.com>
2019-04-27 22:11:32 +0800

    feat: init demo 100%

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
-
-const figlet = require('figlet');
 const chalk = require('chalk');
-
 const program = require('commander');
 const pkg = require('../package');
 
 program
   .version(pkg.version)
   .command('init [type]')
-  .description('run setup commands for all envs')
-  .action(function(type){
+  .description('Init React or Vue project!')
+  .action(function (type) {
     require('../lib/init')(type);
   });
 
-console.log(process.argv,'==process.argv');
-
 program.parse(process.argv);
-
 
 if (!program.args.length) program.help();
